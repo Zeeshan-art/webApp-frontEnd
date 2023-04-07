12282c97bf9167998faeec1643e26eac91fdbb27
Muhammad Zeeshan <zeeshanidrees899@gmail.com>
2023-04-08 00:34:51 +0500

    Banner component complete

--- frontend/src/Banner/Banner.jsx
+++ frontend/src/Banner/Banner.jsx
@@ -1,26 +1,27 @@
 import React from 'react'
 import logo from '../asset/Group 2345.png'
+import image from '../asset/IMAGE.png'
 import '../Banner/style.css'
 
 const Banner = () => {
-    return ( 
+    return (
         <div className='rectangle-1'>
             <div className='banner'>
                 <div className='group-2444'>
                     <div className='rect-1'>
                         <div className='header'>
                             <div className="logo">
-                                <img src={logo} alt=""/>
+                                <img src={logo} alt="" />
                             </div>
 
-                                <ul className='header-menu'>
-                                    <li>Home</li>
-                                    <li>About</li>
-                                    <li>Blog</li>
-                                    <li>Pages</li>
-                                    <li>Contact</li>
+                            <ul className='header-menu'>
+                                <li>Home</li>
+                                <li>About</li>
+                                <li>Blog</li>
+                                <li>Pages</li>
+                                <li>Contact</li>
 
-                                </ul>
+                            </ul>
 
 
                             <div className='help-btn'>
@@ -35,6 +36,22 @@ const Banner = () => {
                         </div>
 
                     </div>
+
+                    <div className='rect-2'>
+                        <div className='group-2385'>
+                            <div className='title'>
+                                <span className='text-1'>Welcome to the</span>
+                                <span className='text-2'>Figma Community</span>
+                            </div>
+                            <div className='group-2443'>
+                                <img src={image} alt='Image' />
+                                <div className='rect-search'></div>
+                                <div className='side-rect'/>
+                                <span className='hint-search'>Search</span>
+                            </div>
+                        </div>
+                    </div>
+
 
                 </div>
             </div>
